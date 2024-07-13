fdf8688d25de26e3885e36f7d8fd878b0c42ceac
Mein <saisainghminetun@gmail.com>
2024-07-13 19:34:30 +0630

    Initial commit

---
+++ app/src/main/java/it/saimao/p2_font_converter/MainActivity.java
@@ -0,0 +1,18 @@
+package it.saimao.p2_font_converter;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
