769629f8f999577f96e661b3e40bfd4a07fb2949
Mein <saisainghminetun@gmail.com>
2024-07-14 10:07:59 +0630

    Added Edit Text and Buttons

--- app/src/main/java/it/saimao/p2_font_converter/MainActivity.java
+++ app/src/main/java/it/saimao/p2_font_converter/MainActivity.java
@@ -1,7 +1,10 @@
 package it.saimao.p2_font_converter;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.CompoundButton;
+import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Toast;
@@ -16,6 +19,8 @@ public class MainActivity extends AppCompatActivity {
 
     private RadioGroup rg;
     private RadioButton rbZg2Uni, rbUni2Zg;
+    private EditText etInput, etOutput;
+    private Button btConvert, btCopy, btClear;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,11 +35,24 @@ public class MainActivity extends AppCompatActivity {
 
         rbZg2Uni.setOnCheckedChangeListener((buttonView, isChecked) -> Toast.makeText(MainActivity.this, "Zg 2 Uni is checked " + isChecked, Toast.LENGTH_SHORT).show());
 
+        btConvert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String inputText = etInput.getText().toString();
+                etOutput.setText(inputText);
+            }
+        });
+
     }
 
     private void initUi() {
         rg = findViewById(R.id.rg_fonts);
         rbZg2Uni = findViewById(R.id.rb_zg2uni);
         rbUni2Zg = findViewById(R.id.rb_uni2zg);
+        etInput = findViewById(R.id.et_input);
+        etOutput = findViewById(R.id.et_output);
+        btClear = findViewById(R.id.bt_clear);
+        btCopy = findViewById(R.id.bt_copy);
+        btConvert = findViewById(R.id.bt_convert);
     }
 }
